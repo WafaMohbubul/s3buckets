849583d8daa577801dca1f1753843dd52ddf1acc
wafam <wafa.mohbubul@hotmail.com>
2023-10-05 10:06:41 +0100

    latest change

--- delete_bucket.py
+++ delete_bucket.py
@@ -1,3 +1,4 @@
+#DELETE BUCKET
 import boto3
 
 s3 = boto3.client('s3')
@@ -6,3 +7,12 @@ bucket_name = "tech254-wafa-bucket"
 
 s3.delete_bucket(Bucket=bucket_name)
 
+
+#DELETE FILE
+import boto3
+
+s3 = boto3.client('s3')
+
+response = s3.delete_object(Bucket="tech254-wafa-bucket", Key="example.txt")
+
+print(response)
